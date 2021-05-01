69cbb2e1e906bf7d3108365b66104165fe8ff87a
Robin Diddams <rdiddams@pinpoint.com>
2021-05-01 17:15:09 -0500

    Deliniate rune vs string methods, allow loading values with NewTrie

--- trie.go
+++ trie.go
@@ -24,21 +24,36 @@ type Trie struct {
 }
 
 // NewTrie is the correct way to init a Trie
-func NewTrie() Trie {
-	return Trie{
+func NewTrie(values ...[]rune) Trie {
+	t := Trie{
 		root: newNode(),
 	}
+	for _, value := range values {
+		t.Load(value)
+	}
+	return t
+}
+
+// NewTrieString is the same as NewTrie but accepts strings instead of []runes
+func NewTrieString(values ...string) Trie {
+	t := Trie{
+		root: newNode(),
+	}
+	for _, value := range values {
+		t.LoadString(value)
+	}
+	return t
 }
 
 // Load adds a []rune to the Trie
-func (t *Trie) Load(emoji []rune) {
-	if len(emoji) == 1 {
-		t.root.next[emoji[0]] = newTerminalNode()
+func (t *Trie) Load(value []rune) {
+	if len(value) == 1 {
+		t.root.next[value[0]] = newTerminalNode()
 		return
 	}
 	current := t.root
-	for i, r := range emoji {
-		if i == len(emoji)-1 {
+	for i, r := range value {
+		if i == len(value)-1 {
 			// last element
 			if current.next[r] == nil {
 				current.next[r] = newTerminalNode()
@@ -55,16 +70,28 @@ func (t *Trie) Load(emoji []rune) {
 	}
 }
 
+// LoadString is a convenience for t.Load([]rune(str))
+func (t *Trie) LoadString(str string) {
+	t.Load([]rune(str))
+}
+
 // SearchCallback is a function called when a match is found
 // return false to keep searching
 type SearchCallback func(hit []rune, at int) (done bool)
 
-// Search searches s for any matches in the trie, returns the first n matches
-func (t *Trie) Search(s string, n int) [][]rune {
+// SearchStringCallback is a function called when a match is found
+// return false to keep searching
+type SearchStringCallback func(hit string, at int) (done bool)
+
+// SearchN searches s for any matches in the trie, returns the first n matches
+func (t *Trie) SearchN(s []rune, n int) [][]rune {
 	var found [][]rune
+	if n < 1 {
+		return found
+	}
 	t.SearchF(s, func(hit []rune, _ int) bool {
 		found = append(found, hit)
-		if n > 0 && len(found) >= n {
+		if len(found) >= n {
 			return true
 		}
 		return false
@@ -72,10 +99,45 @@ func (t *Trie) Search(s string, n int) [][]rune {
 	return found
 }
 
-// SearchF searches s for trie matches and calls cb when it hits one,
+// SearchStringN searches s for any matches in the trie, returns the first n matches
+func (t *Trie) SearchStringN(s string, n int) []string {
+	var found []string
+	if n < 1 {
+		return found
+	}
+	t.SearchStringF(s, func(hit string, _ int) bool {
+		found = append(found, hit)
+		if len(found) >= n {
+			return true
+		}
+		return false
+	})
+	return found
+}
+
+// Search searches s and returns any matches
+func (t *Trie) Search(s []rune) [][]rune {
+	var found [][]rune
+	t.SearchF(s, func(hit []rune, _ int) bool {
+		found = append(found, hit)
+		return false
+	})
+	return found
+}
+
+// SearchString searches s and returns any matches
+func (t *Trie) SearchString(s string) []string {
+	var found []string
+	t.SearchStringF(s, func(hit string, _ int) bool {
+		found = append(found, hit)
+		return false
+	})
+	return found
+}
+
+// SearchF searches input for trie matches and calls cb when it hits one,
 // it will continue until cb returns true
-func (t *Trie) SearchF(s string, cb SearchCallback) {
-	input := []rune(s)
+func (t *Trie) SearchF(input []rune, cb SearchCallback) {
 	var done bool
 	for i := 0; i < len(input) && !done; i++ {
 		r := input[i]
@@ -103,4 +165,12 @@ func (t *Trie) SearchF(s string, cb SearchCallback) {
 			}
 		}
 	}
+}
+
+// SearchStringF searches s for trie matches and calls cb when it hits one,
+// it will continue until cb returns true
+func (t *Trie) SearchStringF(s string, cb SearchStringCallback) {
+	t.SearchF([]rune(s), func(hit []rune, at int) bool {
+		return cb(string(hit), at)
+	})
 }
--- trie_test.go
+++ trie_test.go
@@ -4,51 +4,102 @@ import (
 	"testing"
 )
 
-func assertLen(t *testing.T, slice [][]rune, length int) {
+func assertLenRuneArr(t *testing.T, slice [][]rune, length int) {
 	if len(slice) != length {
 		t.Fatal("slice", slice, "should have length", length, "but has length", len(slice))
 	}
 }
+
+func assertLenString(t *testing.T, slice []string, length int) {
+	if len(slice) != length {
+		t.Fatal("slice", slice, "should have length", length, "but has length", len(slice))
+	}
+}
+
 func assertEqual(t *testing.T, a []rune, b []rune) {
 	if string(a) != string(b) {
 		t.Fatal("values are not equal:", string(a), string(b))
 	}
 }
 
-func TestTrie(t *testing.T) {
-	trie := NewTrie()
-	thermometer := []rune{0x1f321, 0xfe0f}
-	violin := []rune{0x1f3bb}
-	trie.Load(thermometer)
-	trie.Load(violin)
-	emojis := trie.Search("I love to play the "+string(thermometer)+" while i eat a "+string(violin), 2)
-	assertLen(t, emojis, 2)
+func assertEqualString(t *testing.T, a string, b string) {
+	if a != b {
+		t.Fatal("values are not equal:", string(a), string(b))
+	}
 }
 
-func TestTrieLimit(t *testing.T) {
-	trie := NewTrie()
+func TestLoad(t *testing.T) {
+	trie := NewTrie([]rune{'a', 'b'})
+	matches := trie.SearchString("abc")
+	assertLenString(t, matches, 1)
+}
+
+func TestLoadString(t *testing.T) {
+	trie := NewTrieString("ab")
+	matches := trie.SearchString("abc")
+	assertLenString(t, matches, 1)
+}
+
+func TestTrie(t *testing.T) {
 	thermometer := []rune{0x1f321, 0xfe0f}
 	violin := []rune{0x1f3bb}
-	trie.Load(thermometer)
-	trie.Load(violin)
-	emojis := trie.Search("I love to play the "+string(thermometer)+"while i eat a "+string(violin), 1)
-	assertLen(t, emojis, 1)
-	emojis = trie.Search("I love to play the "+string(thermometer)+"while i eat a "+string(violin), -1)
-	assertLen(t, emojis, 2)
+	trie := NewTrie(thermometer, violin)
+	emojis := trie.SearchStringN("I love to play the "+string(thermometer)+" while i eat a "+string(violin), 2)
+	assertLenString(t, emojis, 2)
+}
+
+func TestSearch(t *testing.T) {
+	thermometer := []rune{0x1f321, 0xfe0f}
+	violin := []rune{0x1f3bb}
+	trie := NewTrie(thermometer, violin)
+	emojis := trie.Search([]rune("I love to play the " + string(thermometer) + " while i eat a " + string(violin)))
+	assertLenRuneArr(t, emojis, 2)
+	assertEqual(t, emojis[0], thermometer)
+	assertEqual(t, emojis[1], violin)
+}
+
+func TestSearchN(t *testing.T) {
+	thermometer := []rune{0x1f321, 0xfe0f}
+	violin := []rune{0x1f3bb}
+	trie := NewTrie(thermometer, violin)
+	emojis := trie.SearchN([]rune("I love to play the "+string(thermometer)+"while i eat a "+string(violin)), 1)
+	assertLenRuneArr(t, emojis, 1)
+	emojis = trie.SearchN([]rune("I love to play the "+string(thermometer)+"while i eat a "+string(violin)), 2)
+	assertLenRuneArr(t, emojis, 2)
+	emojis = trie.SearchN([]rune("I love to play the "+string(thermometer)+"while i eat a "+string(violin)), 0)
+	assertLenRuneArr(t, emojis, 0)
+}
+
+func TestSearchStringN(t *testing.T) {
+	thermometer := []rune{0x1f321, 0xfe0f}
+	violin := []rune{0x1f3bb}
+	trie := NewTrie(thermometer, violin)
+	emojis := trie.SearchStringN("I love to play the "+string(thermometer)+"while i eat a "+string(violin), 1)
+	assertLenString(t, emojis, 1)
+	emojis = trie.SearchStringN("I love to play the "+string(thermometer)+"while i eat a "+string(violin), 2)
+	assertLenString(t, emojis, 2)
+}
+
+func TestSearchStringNZero(t *testing.T) {
+	thermometer := []rune{0x1f321, 0xfe0f}
+	violin := []rune{0x1f3bb}
+	trie := NewTrie(thermometer, violin)
+	emojis := trie.SearchStringN("I love to play the "+string(thermometer)+"while i eat a "+string(violin), 0)
+	assertLenString(t, emojis, 0)
+	emojis = trie.SearchStringN("I love to play the "+string(thermometer)+"while i eat a "+string(violin), -100)
+	assertLenString(t, emojis, 0)
 }
 
 func TestZWJEmoji(t *testing.T) {
-	trie := NewTrie()
 	femaleSign := []rune{0x2640, 0xfe0f}
 	// 129335, 8205, 9792, 65039
 	womanShrugging := []rune{0x1f937, 0x200d, 0x2640, 0xfe0f}
-	trie.Load(womanShrugging)
-	trie.Load(femaleSign)
+	trie := NewTrie(womanShrugging, femaleSign)
 	var emojis [][]rune
-	trie.SearchF("fix tests!! or did I "+string(womanShrugging), func(hit []rune, _ int) bool {
+	trie.SearchF([]rune("fix tests!! or did I "+string(womanShrugging)), func(hit []rune, _ int) bool {
 		emojis = append(emojis, hit)
 		return false
 	})
-	assertLen(t, emojis, 1)
+	assertLenRuneArr(t, emojis, 1)
 	assertEqual(t, womanShrugging, emojis[0])
 }
